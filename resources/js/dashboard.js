9d1a3bfb80b84a46c211afd223745471329cdea0
muhammadharrris <muhammadharis72729@gmail.com>
2022-08-24 14:11:30 +0500

    Vip Dashboard Commit

---
+++ resources/js/dashboard.js
@@ -0,0 +1,41 @@
+window.onload = function () {
+
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,  
+        title:{
+            text: "The growth of structure"
+        },
+        axisY: {
+            // title: "Revenue in USD",
+            valueFormatString: "#0,."
+        },
+        data: [{
+            type: "splineArea",
+            color: '#2395F4',
+            markerSize: 5,
+            xValueFormatString: "YYYY",
+            yValueFormatString: "$#,##0.##",
+            dataPoints: [
+                { x: new Date(2000, 0), y: 3289000 },
+                { x: new Date(2001, 0), y: 3830000 },
+                { x: new Date(2002, 0), y: 2009000 },
+                { x: new Date(2003, 0), y: 2840000 },
+                { x: new Date(2004, 0), y: 2396000 },
+                { x: new Date(2005, 0), y: 1613000 },
+                { x: new Date(2006, 0), y: 2821000 },
+                { x: new Date(2007, 0), y: 2000000 },
+                { x: new Date(2008, 0), y: 1397000 },
+                { x: new Date(2009, 0), y: 2506000 },
+                { x: new Date(2010, 0), y: 2798000 },
+                { x: new Date(2011, 0), y: 3386000 },
+                { x: new Date(2012, 0), y: 6704000},
+                { x: new Date(2013, 0), y: 6026000 },
+                { x: new Date(2014, 0), y: 2394000 },
+                { x: new Date(2015, 0), y: 1872000 },
+                { x: new Date(2016, 0), y: 2140000 }
+            ]
+        }]
+        });
+    chart.render();
+    
+}
